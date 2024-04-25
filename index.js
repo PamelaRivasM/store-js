241b401be3d8adc5fe4cd22fa6f69da0ecb83582
PamelaRivasM <164282423+PamelaRivasM@users.noreply.github.com>
2024-04-24 22:08:10 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let totalapagar = 0; // Inicialización de la variante que acumula el total a pagar
-for (let index = 1; index <= 3; index++) {
+const comprar = ()=> {
+    let totalapagar = 0; // Inicialización de la variante que acumula el total a pagar
+    for (let index = 1; index <= 3; index++) {
     const nombre = prompt("¿Qué producto desea llevar?"); // Solicitar el nombre del producto
     let cantidad = Number(prompt("¿Cuántas unidades?")); //Solicitar la cantidad y convertir a numero
     let precio = Number(prompt("¿Cuánto sale cada unidad?")); //Solicitar el precio y convertir a número
 
     const subtotal = cantidad * precio; // Calcular el subtotal
-    totalapagar += subtotal; // Sumar el subtotal el total a pagar
+    totalapagar = totalapagar + subtotal; // Sumar el subtotal el total a pagar
+    }
+
+    console.log(totalapagar); // Mostrar el total a pagar
+    return totalapagar;
 }
 
-console.log(``); // Mostrar el total a pagar
-alert("El resultado a pagar es: "+ totalapagar );
+//const total1 = comprar();
+//const total2 = comprar();  Forma 1
+//const total3 = comprar();
+//const total = total1 + total2 + total3;
+
+const total = comprar() + comprar() + comprar(); 
